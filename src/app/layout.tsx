8363d09f3c50f4457bc886ebc4a6fc24546257b9
Bahar <ylmzbhr1@gmail.com>
2025-09-22 02:56:20 +0300

    Remove unused @next/font dependency and associated preconnect links from layout. Update package-lock and package.json accordingly.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,12 +43,6 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Single+Day&display=swap"
           rel="stylesheet"
